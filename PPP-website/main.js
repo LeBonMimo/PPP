ed9adf43f78a78f2ba11c1abb83f2c47823dbe32
Guillaume SCHWAB <schwab.guillaume2310@gmail.com>
2024-03-15 09:30:57 +0100

    comment GSAP

--- PPP-website/main.js
+++ PPP-website/main.js
@@ -1,108 +1,90 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
-
-const PPPContainer = document.querySelector(".container");
-
-function removeElement(element) {
-  if (typeof(element) === "string") {
-    element = document.querySelector(element);
-  }
-  return function() {
-    element.parentNode.removeChild(element);
-  };
-}
-
-let tl = gsap.timeline();
-
-tl.to(".hero", {
-  scrollTrigger: {
-  trigger: ".hero",
-  start: "top center",
-  end: "center top",
-  toggleClass: "visible"
-  }
-})
-
-tl.to(".section1", {
-  scrollTrigger: {
-  trigger: ".section1",
-  start: "top center",
-  end: "top top",
-  toggleClass: "visible"
-  }
-})
-
-.to(".ouf", {
-  scrollTrigger: {
-      trigger: ".ouf",
-      start: "bottom center",
-      toggleClass: "fs-800",
-    }
-  })
 
 
-  .to(".ouf", {
-    scrollTrigger: {
-      trigger: ".ouf2",
-      start: "bottom center",
-      toggleClass: "fs-800"
-    }
-  })
-
-  .to(".ouf", {
-    scrollTrigger: {
-      trigger: ".antoufle",
-      start: "bottom center",
-      toggleClass: "fs-800"
-    }
-  })
-
-  .to(".antoufle", {
-    scrollTrigger: {
-      trigger: ".hero-text",
-      start: "top center",
-      toggleClass: "height-auto"
-    }
-  })
-  .to(".antoufle", {
-    scrollTrigger: {
-      trigger: ".hero-text",
-      start: "top center",
-      toggleClass: "visible"
-    }
-  })
 
 
-tl.to(".section2", {
-  scrollTrigger: {
-  trigger: ".section2",
-  start: "top center",
-  end: "top top",
-  toggleClass: "visible"
-  }
-})
 
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+// import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-// setupCounter(document.querySelector('#counter'))
+// gsap.registerPlugin(ScrollTrigger);
+
+// const PPPContainer = document.querySelector(".container");
+
+// function removeElement(element) {
+//   if (typeof(element) === "string") {
+//     element = document.querySelector(element);
+//   }
+//   return function() {
+//     element.parentNode.removeChild(element);
+//   };
+// }
+
+// let tl = gsap.timeline();
+
+// tl.to(".hero", {
+//   scrollTrigger: {
+//   trigger: ".hero",
+//   start: "top center",
+//   end: "center top",
+//   toggleClass: "visible"
+//   }
+// })
+
+// tl.to(".section1", {
+//   scrollTrigger: {
+//   trigger: ".section1",
+//   start: "top center",
+//   end: "top top",
+//   toggleClass: "visible"
+//   }
+// })
+
+// .to(".ouf", {
+//   scrollTrigger: {
+//       trigger: ".ouf",
+//       start: "bottom center",
+//       toggleClass: "fs-800",
+//     }
+//   })
+
+
+//   .to(".ouf", {
+//     scrollTrigger: {
+//       trigger: ".ouf2",
+//       start: "bottom center",
+//       toggleClass: "fs-800"
+//     }
+//   })
+
+//   .to(".ouf", {
+//     scrollTrigger: {
+//       trigger: ".antoufle",
+//       start: "bottom center",
+//       toggleClass: "fs-800"
+//     }
+//   })
+
+//   .to(".antoufle", {
+//     scrollTrigger: {
+//       trigger: ".hero-text",
+//       start: "top center",
+//       toggleClass: "height-auto"
+//     }
+//   })
+//   .to(".antoufle", {
+//     scrollTrigger: {
+//       trigger: ".hero-text",
+//       start: "top center",
+//       toggleClass: "visible"
+//     }
+//   })
+
+
+// tl.to(".section2", {
+//   scrollTrigger: {
+//   trigger: ".section2",
+//   start: "top center",
+//   end: "top top",
+//   toggleClass: "visible"
+//   }
+// })
